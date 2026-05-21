299bb1dde4826c9c2523eec524ff4e2358b35bbc
Shubham Sharma <shubham.asr101@gmail.com>
2026-05-21 19:39:14 +0530

    Minor changes

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -16,7 +16,7 @@ const UserCard = ({ user }) => {
       );
       dispatch(removeUserFromFeed(userId));
     } catch (err) {
-      console.error(err)
+      console.error("err:::",err)
     }
   };
 
